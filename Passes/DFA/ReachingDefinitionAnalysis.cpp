609e78a6d708d2bdce46686e4c9ef3e9ad38efea
llcheng94 <llcheng94@163.com>
2018-05-25 22:04:34 +0800

    implement RDA class and pass function

--- Passes/DFA/ReachingDefinitionAnalysis.cpp
+++ Passes/DFA/ReachingDefinitionAnalysis.cpp
@@ -1,6 +1,7 @@
 #include "231DFA.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include <string.h>
 
 using namespace llvm;
 using namespace std;
@@ -48,13 +49,71 @@ public:
 
 };
 
+class ReachingDefinitionAnalysis: public DataFlowAnalysis<ReachingInfo, true> {
+private:
+  typedef pair<unsigned, unsigned> Edge;
+  map<Edge, ReachingInfo*> EdgeToInfo;
+  map<string, int> opTocategory = {{"br", 2}, {"switch", 2}, {"alloca", 1}, {"load", 1},\
+                                   {"store", 2}, {"getelementptr", 1}, {"icmp", 1}, {"fcmp", 1},\
+                                   {"phi", 3}, {"select", 1}
+                                  };
+
+public:
+  ReachingDefinitionAnalysis(ReachingInfo & bottom, ReachingInfo & initialState):\
+    DataFlowAnalysis(bottom, initialState) {}
+
+    void flowfunction(Instruction * I,
+                      vector<unsigned> & IncomingEdges,
+                      vector<unsigned> & OutgoingEdges,
+                      vector<ReachingInfo *> & Infos){
+
+      string opName = I->getOpcodeName();
+      int category = opTocategory.count(opName) ? opTocategory[opName] : 2;
+      category = I->isBinaryOp() ? 1 : category;
+
+      unsigned instrIdx = getInstrToIndex()[I];
+      map<Edge, ReachingInfo*> edgeToInfo = getEdgeToInfo();
+      ReachingInfo* info = new ReachingInfo();
+
+      for (size_t i = 0; i < IncomingEdges.size(); i++) {
+        Edge inEdge = make_pair(IncomingEdges[i], instrIdx);
+        ReachingInfo::join(info, edgeToInfo[inEdge], info);
+      }
+
+      if(category == 1){
+        set<unsigned> current = {instrIdx};
+        ReachingInfo::join(info, new ReachingInfo(current), info);
+      }
+      else if(category == 3){
+        Instruction* firstNonPhi = I->getParent()->getFirstNonPHI();
+        unsigned firstNonPhiIdx = getInstrToIndex()[firstNonPhi];
+        set<unsigned> current;
+
+        for (size_t i = instrIdx; i < firstNonPhiIdx; i++) {
+          current.insert(i);
+        }
+
+        ReachingInfo::join(info, new ReachingInfo(current), info);
+      }
+
+      for (size_t i = 0; i < OutgoingEdges.size(); i++) {
+        Infos.push_back(info);
+      }
+    }
+};
+
 namespace {
   struct ReachingDefinitionAnalysisPass: public FunctionPass {
     static char ID;
 
     ReachingDefinitionAnalysisPass() : FunctionPass(ID) {}
     bool runOnFunction(Function &F) override {
-      errs() << "ReachingDefinitionAnalysis.\n";
+      ReachingInfo bottom, initialState;
+      ReachingDefinitionAnalysis* RDA = new ReachingDefinitionAnalysis(bottom, initialState);
+
+      RDA->runWorklistAlgorithm(&F);
+      RDA->print();
+
       return false;
     }
   };
